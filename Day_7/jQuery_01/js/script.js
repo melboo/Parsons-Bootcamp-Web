ebdde2fadb177f2c7de505df12602e009b8339e3
melboo <mailanmelanie@gmail.com>
2014-08-05 07:29:19 -0400

    up

--- Day_7/jQuery_01/js/script.js
+++
@@ -1,6 +0,0 @@
-/*Javascript/jQuery here*/
-
-$(document).ready(function() {
-  //alert('Ready to build your things!');
-  console.log('my consol for debugg');
-});
---
+++ Day_8/examples/jQuery_01/js/script.js
@@ -0,0 +1,21 @@
+/*Javascript/jQuery here*/
+
+$(document).ready(function() {
+  //alert('Ready to build your things!');
+  //console.log('my consol for debugg');
+
+
+  //which object	.	which function
+  $("#click_me").click(function(){
+  	//what happends..
+  	//get object with id 	. do function 	(parameter)
+  	
+  	//$("#main_nav").slideToggle("slow");
+
+  	});
+
+
+$("#click_me").mouseover(function(){
+		$("#click_me").css("background-color", "blue");
+	});
+});
