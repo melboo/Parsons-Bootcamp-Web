ddb898efc4777f20a6a4a1c28368623a21a2a732
melboo <mailanmelanie@gmail.com>
2014-08-13 18:46:17 -0400

    up

--- Day_14/examples/JS_parallax_wynn/js/script.js
+++ Day_14/examples/JS_parallax_wynn/js/script.js
@@ -4,8 +4,11 @@ function parallex() {
 	var layer1 = document.getElementById("layer1");
 	var layer2 = document.getElementById("layer2");
 	//page X = mouse X
-	layer1.style.right = -(event.pageX / 40)+"px";
-	console.log(window.pageYOffset);
+	layer1.style.right = -(event.pageX / 4)+"px";
+	layer1.style.top = -(event.pageX / 2)+"px";
+
+	console.log(event.pageX);
+
 	layer2.style.top = -(event.pageX / 8) + "px";
 }
 
