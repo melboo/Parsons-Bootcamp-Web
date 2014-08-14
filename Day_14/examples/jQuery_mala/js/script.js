8707abe39eedc204432b19b7da2f3617fc19cd59
melboo <mailanmelanie@gmail.com>
2014-08-13 20:57:10 -0400

    up

---
+++ Day_14/examples/jQuery_mala/js/script.js
@@ -0,0 +1,38 @@
+/*JavaScript here*/
+
+/*Welcome to jQuery*/
+
+$(document).ready(function(){
+  $("#why-link").click(function(){
+    $("#why").slideToggle("slow");
+    // $("#why").slideDown();
+    // $("#what").slideUp();
+    // $("#where").slideUp();
+    // $("#where").css("display","none");
+    // $("#what").css("display","none");
+
+  });
+
+  $("#what-link").click(function(){
+    $("#what").slideToggle("slow");
+    // $("#what").slideDown();
+    // $("#why").slideUp();
+    // $("#where").slideUp();
+    // $("#where").css("display","none");
+    // $("#why").css("display","none");
+  });
+
+  $("#where-link").click(function(){
+    $("#where").slideToggle("slow");
+    // $("#where").slideDown();
+    // $("#what").slideUp();
+    // $("#why").slideUp();
+    // $("#why").css("display","none");
+    // $("#what").css("display","none");
+  });
+
+
+
+
+});
+
